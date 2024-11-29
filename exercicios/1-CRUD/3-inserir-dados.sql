60768927545b0b5dd7e065afe72399094af4fbeb
GustavoHCS001 <141564911+GustavoHCS001@users.noreply.github.com>
2024-11-29 15:05:57 +0000

    ex 3 4 5

--- exercicios/1-CRUD/3-inserir-dados.sql
+++ exercicios/1-CRUD/3-inserir-dados.sql
@@ -1,4 +1,7 @@
 -- Insira 3 classes salariais na tabela salario_classe
-
+INSERT INTO salario_classe(id_salario, salario, nivel)
+VALUES 
+(4, 8500, 'gerencia'),
+(5, 107000, 'coordenação');
 
 -- Insira 1 nova classe salarial, sem declarar o atributo id_salario
