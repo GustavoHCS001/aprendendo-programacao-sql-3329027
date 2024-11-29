610d5d1a2b1c3330d1a08abbb2caf25d786f2acc
GustavoHCS001 <141564911+GustavoHCS001@users.noreply.github.com>
2024-11-29 18:45:45 +0000

    ex_3 1 2 3

--- exercicios/4-refinando-consulta/2-group-by-e-having.sql
+++ exercicios/4-refinando-consulta/2-group-by-e-having.sql
@@ -1,7 +1,32 @@
 -- Crie uma consulta usando as tabelas "invoices" e "customers" cujo o resultado seja o total de gastos por cliente
-
-
+SELECT 
+cus.CustomerId AS id_cliente,
+cus.FirstName AS nome,
+SUM(inv.Total) AS 'Total gasto'
+FROM
+invoices AS inv
+INNER JOIN customers AS cus ON inv.CustomerId = cus.CustomerId
+GROUP BY id_cliente;
 -- Na consulta anterior, insira um filtro para que apresente apenas os clientes que gastaram mais de R$30
-
+SELECT 
+cus.CustomerId AS id_cliente,
+cus.FirstName AS nome,
+SUM(inv.Total) AS 'Total gasto'
+FROM
+invoices AS inv
+INNER JOIN customers AS cus ON inv.CustomerId = cus.CustomerId
+GROUP BY id_cliente
+HAVING SUM(inv.Total) >=40;
+-- Na consulta anteri
+SELECT 
+cus.CustomerId AS id_cliente,
+cus.FirstName AS nome,
+SUM(inv.Total) AS 'Total gasto',
+COUNT(inv.Total) AS 'Quantidade Compras'
+FROM
+invoices AS inv
+INNER JOIN customers AS cus ON inv.CustomerId = cus.CustomerId
+GROUP BY id_cliente
+HAVING SUM(inv.Total) >=40;
 
 -- Na consulta anterior, insira uma coluna que informe a compra de maior valor feita por cada cliente
--- exercicios/4-refinando-consulta/3-ordenando-e-limitando.sql
+++ exercicios/4-refinando-consulta/3-ordenando-e-limitando.sql
@@ -1,7 +1,40 @@
 -- Desenvolva uma consulta para a tabela "customers" que retorne apenas 10 linhas
-
+SELECT
+CustomerId AS id,
+FirstName AS Nome,
+Country AS Pais
+FROM
+customers
+LIMIT 10;
 
 -- Usando a consulta anterior, ordene a coluna "Country" de forma crescente e "FirstName" de forma decrescente
-
+SELECT
+CustomerId AS id,
+FirstName AS Nome,
+Country AS Pais
+FROM
+customers
+LIMIT 10 OFFSET 3;
 
 -- Continue usando a consulta anterior, dessa vez retorne 10 resultados, no entando ignore as 3 primeiras linhas
+SELECT
+CustomerId AS id,
+FirstName AS Nome,
+Country AS Pais
+FROM
+customers
+ORDER BY
+Pais ASC
+LIMIT 10 OFFSET 3;
+
+--
+SELECT
+CustomerId AS id,
+FirstName AS Nome,
+Country AS Pais
+FROM
+customers
+ORDER BY
+Pais ASC
+Nome DESC 
+LIMIT 10;
